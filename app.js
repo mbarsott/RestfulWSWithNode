ea57c70087ac0dfc3b0664d38830d2d6d8449a4f
Marcelo Barsotti <mbarsott@gmail.com>
2019-11-10 20:35:03 -0500

    2. What is REST

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Welcome to my API!");
+  res.send("Welcome to my Nodemon API!");
 });
 
-app.listen(port, _ => {
-  console.log("Running on port " + port);
+app.listen(port, () => {
+  console.log(`Running on port ${port}`); // eslint-disable-line
 });
