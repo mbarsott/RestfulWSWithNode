32d495388e9a6c9406772ae1bd994aac6f785b34
Marcelo Barsotti <mbarsott@gmail.com>
2019-12-06 21:47:39 -0500

    6. Testing

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -1,27 +1,13 @@
 const express = require("express");
+const booksController = require("../controllers/booksController");
 
 function routes(Book) {
   const bookRouter = express.Router();
+  const controller = booksController(Book);
   bookRouter
     .route("/books")
-    .post((req, res) => {
-      const book = new Book(req.body);
-      book.save();
-      console.log(book);
-      return res.status(201).json(book);
-    })
-    .get((req, res) => {
-      const query = {};
-      if (req.query.genre) {
-        query.genre = req.query.genre;
-      }
-      Book.find(query, (err, books) => {
-        if (err) {
-          return res.send(err);
-        }
-        return res.json(books);
-      });
-    });
+    .post(controller.post)
+    .get(controller.get);
 
   bookRouter.use("/books/:bookId", (req, res, next) => {
     Book.findById(req.params.bookId, (err, book) => {
@@ -71,7 +57,7 @@ function routes(Book) {
       });
     })
     .delete((req, res) => {
-      req.book.remove((err) => {
+      req.book.remove(err => {
         if (err) {
           return res.send(err);
         }
